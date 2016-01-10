ee1d520a949a860ce6d51c02ee40ac7ac8158868
simonbaas <simon.baas@gmail.com>
2016-01-10 12:47:12 +0100

    Added log methods to ILogger interface. Fixed NULL logger implementation.

--- src/EasyZMq/EasyZMq/Logging/EasyZMqNullLogger.cs
+++ src/EasyZMq/EasyZMq/Logging/EasyZMqNullLogger.cs
@@ -1,6 +1,82 @@
-﻿namespace EasyZMq.Logging
+﻿using System;
+
+namespace EasyZMq.Logging
 {
     public class EasyZMqNullLogger : ILogger
     {
+        public void Debug(string message)
+        {
+            
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            
+        }
+
+        public void Debug(string message, Exception exception)
+        {
+            
+        }
+
+        public void Info(string message)
+        {
+            
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            
+        }
+
+        public void Info(string message, Exception exception)
+        {
+            
+        }
+
+        public void Warning(string message)
+        {
+            
+        }
+
+        public void Warning(string format, params object[] args)
+        {
+            
+        }
+
+        public void Warning(string message, Exception exception)
+        {
+            
+        }
+
+        public void Error(string message)
+        {
+            
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            
+        }
+
+        public void Fatal(string message)
+        {
+            
+        }
+
+        public void Fatal(string format, params object[] args)
+        {
+            
+        }
+
+        public void Fatal(string message, Exception exception)
+        {
+            
+        }
     }
 }
