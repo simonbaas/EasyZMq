d1f0f61f921fcf6bc7a96b8d4d65f28c57c1ae79
simonbaas <simon.baas@gmail.com>
2016-01-30 16:01:24 +0100

    Changed description of test class

--- src/EasyZMq.Tests/PubSubTests.cs
+++ src/EasyZMq.Tests/PubSubTests.cs
@@ -8,7 +8,7 @@ using NUnit.Framework;
 
 namespace EasyZMq.Tests
 {
-    [TestFixture(Author = "Simon Baas", Description = "Publish/Subscribe Tests")]
+    [TestFixture(Author = "Simon Baas", Description = "Publish/Subscribe Sockets Tests")]
     public class PubSubTests
     {
         private static readonly TimeSpan WaitTimeout = TimeSpan.FromSeconds(10);
