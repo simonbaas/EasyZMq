dc20fc887b27955235ea9a678235a4569bfec6a9
Simon Baas <simon.baas@gmail.com>
2017-07-30 09:21:03 +0200

    Updated CreateVersions.sh script

--- deploy/CreateVersions.sh
+++ deploy/CreateVersions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-version=`git describe --tags --long --match ?.?.?`
-branch=`git branch | grep \* | cut -d ' ' -f2`
+version=`git describe --tags --long --match ?.?.?` 
+branch=`echo %build.branch.name% | rev | cut -d/ -f1 | rev`
 buildCounter=%build.counter%
 
 if [[ $version =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)-([a-z0-9]+)$ ]]
