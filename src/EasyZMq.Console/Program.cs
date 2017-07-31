7006e30202b1d3a342f6afdf536afc23374f0f05
simonbaas <simon.baas@gmail.com>
2017-07-31 15:11:06 +0200

    Added Console test application

---
+++ src/EasyZMq.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Monitoring;
+using NetMQ.Sockets;
+
+namespace EasyZMq.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var publisher = new PublisherSocket())
+            {
+                publisher.Bind("tcp://localhost:1234");
+
+                using (var subscriber = new SubscriberSocket())
+                {
+                    subscriber.Subscribe("A");
+
+                    using (var poller = new NetMQPoller { subscriber })
+                    {
+                        using (var monitor = new NetMQMonitor(subscriber, "inproc://sub.inproc", SocketEvents.Connected | SocketEvents.Disconnected | SocketEvents.ConnectRetried))
+                        {
+                            monitor.AttachToPoller(poller);
+
+                            subscriber.Connect("tcp://localhost:1234");
+
+                            poller.RunAsync();
+
+                            System.Console.ReadKey();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
