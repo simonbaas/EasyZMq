8c815cd65ca1f01256e8ae0f83b805585c0decf0
simonbaas <simon.baas@gmail.com>
2017-08-01 08:16:34 +0200

    Updated CreateVersions.sh

--- deploy/CreateVersions.sh
+++ deploy/CreateVersions.sh
@@ -12,15 +12,15 @@ then
     commitCount="${BASH_REMATCH[4]}"
     shortHash="${BASH_REMATCH[5]}"
 
-    assemblyVersion="$majorVersion.$minorVersion.$patchVersion.$commitCount"
+    packageVersion="$majorVersion.$minorVersion.$patchVersion.$commitCount"
 
     echo "##teamcity[setParameter name='system.MajorVersion' value='$majorVersion']"
     echo "##teamcity[setParameter name='system.MinorVersion' value='$minorVersion']"
     echo "##teamcity[setParameter name='system.PatchVersion' value='$patchVersion']"
     echo "##teamcity[setParameter name='system.CommitCount' value='$commitCount']"
-    echo "##teamcity[setParameter name='system.AssemblyVersion' value='$assemblyVersion']"
+    echo "##teamcity[setParameter name='system.PackageVersion' value='$packageVersion']"
     echo "##teamcity[setParameter name='system.ShortHash' value='$shortHash']"
-    echo "##teamcity[buildNumber '$assemblyVersion-$branch.$buildCounter']"
+    echo "##teamcity[buildNumber '$packageVersion-$branch.$buildCounter']"
 else
     echo "$version doesn't match regular expression"
     exit 1
